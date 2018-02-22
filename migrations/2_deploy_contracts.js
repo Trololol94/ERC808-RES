0ed101460416d8feb35b02aae91a6704c0993bfa
Bastien Chevallier <cheval_b@etna-alternance.net>
2018-02-22 16:32:44 +0100

    edit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var AvaibilityStruct = artifacts.require("./AvaibilityStruct.sol");
 
@@ -5,4 +6,13 @@ module.exports = function(deployer) {
 	deployer.deploy(ConvertLib);
 	deployer.link(ConvertLib, AvaibilityStruct);
 	deployer.deploy(AvaibilityStruct);
+=======
+var ERC20 = artifacts.require("zeppelin-solidity/token/ERC20");
+var ERC808 = artifacts.require("./ERC808.sol");
+
+module.exports = function(deployer) {
+	deployer.deploy(ERC20);
+	deployer.link(ERC20, ERC808);
+	deployer.deploy(ERC808);
+>>>>>>> Stashed changes
 };
---
+++ test/ERC808.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.17;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/ERC808.sol";
+
+contract TestERC808 {
+  ERC808 erc;
+  
+  function beforeAll() public {
+    erc = ERC808(DeployedAddresses.ERC808());
+    
+    erc.addAvailabilities(1, 1, 2, ERC808.BookingStatus.REQUESTED, 1, 1500000, 1000000, 2000000, "http://www.lolcat.com");
+    erc.addAvailabilities(2, 1, 8, ERC808.BookingStatus.REJECTED, 1, 1500000, 1000000, 2000000, "http://www.lolipop.com");
+    erc.addAvailabilities(3, 1, 12, ERC808.BookingStatus.CANCELLED, 1, 1500000, 1000000, 2000000, "http://www.puppies.com");
+  }
+  
+  function testGetMetadata() public {
+    Assert.equal(erc.getMetadata(3), "http://www.puppies.com", "Meta is not correct!");
+  }
+
+  function testGetAvailability() public {
+    ERC808.Availability storage availability = erc.getAvailability(1);
+    ERC808.Availability storage expected = (1, 1, 2, ERC808.BookingStatus.REQUESTED, 1, 1500000, 1000000, 2000000, "http://www.lolcat.com");
+    
+    Assert.equal(availability, expected, "Get Availability has a problem");
+  }
+  
+  
+  function testGetAvailabilities() public {
+    ERC808.Availability[] storage availabilities = erc.getAvailabilities();
+    ERC808.Availability[] storage expected = [
+      (1, 1, 2, ERC808.BookingStatus.REQUESTED, 1, 1500000, 1000000, 2000000, "http://www.lolcat.com"),
+      (2, 1, 8, ERC808.BookingStatus.REJECTED, 1, 1500000, 1000000, 2000000, "http://www.lolipop.com"),
+      (3, 1, 12, ERC808.BookingStatus.CANCELLED, 1, 1500000, 1000000, 2000000, "http://www.puppies.com")
+    ];
+    
+    Assert.equal(availabilities, expected, "Get Availabilities has a problem");
+  }
+}
